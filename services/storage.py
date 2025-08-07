4e680db0b44ae07538957b42a868a2031ff03a15
VickiannJC <vickiann.jimenez@gmail.com>
2025-08-06 19:50:46 -0500

    method->put_object-en create_presigned_url->storage.py

--- services/storage.py
+++ services/storage.py
@@ -15,17 +15,13 @@ dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
 s3_client = boto3.client('s3', region_name=AWS_REGION)
 
 # --- Funciones de S3 ---
-def create_presigned_url(bucket_name: str, object_name: str, expiration=604800):
-    """
-    Genera una URL prefirmada explícitamente para subir un archivo (PUT).
-    """
+def create_presigned_url(bucket_name: str, object_name: str, expiration=604800, method='put_object'):
     try:
         response = s3_client.generate_presigned_url(
-            'put_object',
-            Params={'Bucket': bucket_name,
-                    'Key': object_name},
+            ClientMethod=method,
+            Params={'Bucket': bucket_name, 'Key': object_name},
             ExpiresIn=expiration,
-            HttpMethod='PUT'  
+            HttpMethod='PUT'
         )
     except ClientError as e:
         print(f"Error al generar URL prefirmada: {e}")
